58b780c66016b895c0dd096f0c9096c8e01a2cd4
Tsobu Developer <dev@tsobu.co.ke>
2021-04-23 11:53:30 +0300

    Updates

--- hooks/commit-msg-format.sh
+++ hooks/commit-msg-format.sh
@@ -6,22 +6,16 @@
 # may need to make the script executable by 'chmod +x .git/hooks/commit-msg'.
 ################################################################################
 
-#
-# Automatically adds branch name and branch description to every commit message.
-# Modified from the stackoverflow answer here: http://stackoverflow.com/a/11524807/151445
-#
+path=$1
+echo path is $path
 
-# Succeed on all merge messages, as evidenced by MERGE_MSG existing
-[ -f $GIT_DIR/MERGE_MSG ] && exit 0
-
-# Get branch name and description
-NAME=$(git branch | grep '*' | sed 's/* //')
-DESCRIPTION=$(git config branch."$NAME".description)
-
-# Don't apply this logic if we are in a 'detached head' state (rebasing, read-only history, etc)
-# newlines below may need echo -e "\n\n: (etc.)"
-if [ "$NAME" != "(no branch)" ]; then
-	# Append branch name and optional description to COMMIT_MSG
-	# For info on parameters to githooks, run: man githooks
-	echo "\n\n: $NAME $DESCRIPTION" >> "$1"
+a=$(cat $path)
+echo commit message is
+echo $a
+if [[ "$a" =~ "hello world" ]];then
+  echo commit format test passed
+  exit 0
+else
+  echo commit format test failed
+  exit 1
 fi
