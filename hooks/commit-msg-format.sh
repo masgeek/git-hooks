b6764d0ee3aa797a470a59ce2e3b9a1dfbb3e993
Tsobu Developer <dev@tsobu.co.ke>
2021-04-23 11:40:10 +0300

    Updates

--- hooks/commit-msg-format.sh
+++ hooks/commit-msg-format.sh
@@ -6,18 +6,23 @@
 # may need to make the script executable by 'chmod +x .git/hooks/commit-msg'.
 ################################################################################
 
-# set this to your active development branch
-develop_branch="develop"
-current_branch="$(git rev-parse --abbrev-ref HEAD)"
+commit_regex = '^((build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test|Publish)(\([a-zA-Z0-9 /_,.-]+?\))?: .{1,59}\w (\([A-Z]+-[0-9]+\)|\(#[0-9]+\)|\[skip ci\]|\[ci\])|Merge branch .+|(fixup|squash)! .+|WIP: .+)$'
 
-# only check commit messages on main development branch
-# [ "$current_branch" != "$develop_branch" ] && exit 0
+error_msg = '
+.git/hooks/commit-msg: Aborting commit, try again with a valid message. The regexp checks for:
 
-# regex to validate in commit msg
-commit_regex='(wap-[0-9]+|merge)'
-error_msg="Aborting commit. Your commit message is missing either a JIRA Issue ('WAP-1111') or 'Merge'"
+* a conventional commit message subject (to be parsed into changelogs, see https://www.conventionalcommits.org/en/v1.0.0 ):
+   - a change type, either of "build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test" or "Publish"
+   - optionally, in parenthesis, subsystem(s) affected by the change (comma delimited list)
+   - a colon, space (": ") and a brief subject (60 characters maximum) not ending with a period
+   - a space and in parenthesis, an issue id " (FOO-123)" or " (#123)"... or the tags "[skip ci]" or "[ci]" (do we use these?)
+* ... or, a Git autosquash line, i.e. "fixup! My previous commit" or "squash! Another previous commit" (see https://thoughtbot.com/blog/autosquashing-git-commits )
+* ... or, a merge commit "Merge branch x..."
+* ... or, a work-in-progress tag "WIP: "
+* optionally (not validated), two line breaks and a commit message body
+This script is written by CJ Sveningsson (carl-johan.sveningsson@scania.com), inspired by https://gist.github.com/pgilad/5d7e4db725a906bd7aa7 (Gilad Peleg)'
 
-if ! grep -iqE "$commit_regex" "$1"; then
-    echo "$error_msg" >&2
+if ! grep -iqE "${commit_regex}" "$1"; then
+    echo "${error_msg}" >&2
     exit 1
-fi
+f
